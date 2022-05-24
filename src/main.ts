58be153d9f57ed1a6a71388adcb483c28be00780
Martijn Dorsman <martijn.dorsman@gmail.com>
2022-05-24 20:07:59 +0200

    Add flags

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ app.appendChild(board);
 const cell = document.createElement('div');
 cell.classList.add('cell');
 
+// Handle click holding
 let pointerDown = false;
 
 document.addEventListener('pointerdown', () => {
@@ -21,9 +22,8 @@ document.addEventListener('pointerup', () => {
   pointerDown = false;
 })
 
-// Add 100 cells
-for (let i = 0; i < 100; i++) {
-  const cell = document.createElement('div');
+function createCell() {
+  let cell = document.createElement('div');
   cell.classList.add('cell');
   cell.addEventListener('pointerdown', () => {
     cell.classList.add('pressed');
@@ -33,7 +33,7 @@ for (let i = 0; i < 100; i++) {
     if (pointerDown) {
       cell.classList.add('pressed');
     }
-  })
+  });
 
   cell.addEventListener('pointerup', () => {
     cell.classList.remove('pressed');
@@ -41,6 +41,32 @@ for (let i = 0; i < 100; i++) {
 
   cell.addEventListener('pointerleave', () => {
     cell.classList.remove('pressed');
+  });
+
+  cell.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    cell = cell.classList.contains('flag') ? removeFlag(cell) : addFlag(cell);
   })
+  
+  return cell;
+}
+
+function addFlag(cell: HTMLDivElement) {
+  cell.classList.add('flag');
+  cell.innerHTML = 'f';
+  return cell;
+}
+
+function removeFlag(cell: HTMLDivElement) {
+  cell.classList.remove('flag');
+  cell.innerHTML = '';
+  return cell;
+}
+
+// Add 100 cells
+for (let i = 0; i < 100; i++) {
+  const cell = createCell();
   board.appendChild(cell);
 }
+
+
