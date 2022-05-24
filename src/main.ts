a2238041519ed3a0c5221f5d67b763e63aa6dae8
Martijn Dorsman <martijn.dorsman@gmail.com>
2022-05-24 21:11:28 +0200

    Add basic setup

--- src/main.ts
+++ src/main.ts
@@ -41,6 +41,10 @@ function createCell() {
 
   cell.addEventListener('click', () => {
     cell.classList.add('opened');
+    cell.innerHTML = cell.getAttribute('data-bombs-around') ?? '';
+    if (cell.getAttribute('data-has-bomb') === 'true') {
+      cell.innerHTML = 'x';
+    }
   });
 
   cell.addEventListener('pointerenter', () => {
@@ -79,9 +83,8 @@ function removeFlag(cell: HTMLDivElement) {
   return cell;
 }
 
-const map = Array(10).fill(Array(10).fill('x'));
+const map: HTMLDivElement[][] = Array(ROWS).fill(null).map(() => Array(COLS));
 
-let cells: HTMLDivElement[] = [];
 // Add 100 cells
 for (const [rowIndex, rows] of map.entries()) {
   for (const [colIndex] of rows.entries()) {
@@ -89,15 +92,67 @@ for (const [rowIndex, rows] of map.entries()) {
     const cell = createCell();
     cell.setAttribute('x', colIndex.toString());
     cell.setAttribute('y', rowIndex.toString());
-    
+
     const pos = parseInt(rowIndex.toString() + colIndex.toString());
     if (bombIndexes.has(pos)) {
-      cell.setAttribute('has-bomb', 'true');
+      cell.setAttribute('data-has-bomb', 'true');
     }
 
-    cells = [...cells, cell];
+    map[rowIndex][colIndex] = cell;
     board.appendChild(cell);
   }
 }
 
+function setBombsAround(cell: HTMLDivElement, neighbour: HTMLDivElement) {
+  if (neighbour.getAttribute('data-has-bomb') === 'true') {
+    cell.setAttribute('data-bombs-around', (parseInt(cell.getAttribute('data-bombs-around') ?? 0 + '') + 1).toString());
+  }
+}
+
+for (const [rowIndex, rows] of map.entries()) {
+  for (const [colIndex] of rows.entries()) {
+    const cell = map[rowIndex][colIndex];
+    // top - left
+    if (rowIndex > 0 && colIndex > 0) {
+      setBombsAround(cell, map[rowIndex - 1][colIndex - 1]);
+    }
+
+    // top
+    if (rowIndex > 0) {
+      setBombsAround(cell, map[rowIndex - 1][colIndex]);
+    }
+
+    // top - right
+    if (rowIndex > 0 && colIndex < COLS - 1) {
+      setBombsAround(cell, map[rowIndex - 1][colIndex + 1]);
+    }
+
+    // right
+    if (colIndex < COLS - 1) {
+      setBombsAround(cell, map[rowIndex][colIndex + 1]);
+    }
+
+    // bottom - right
+    if (rowIndex < ROWS - 1 && colIndex < COLS - 1) {
+      setBombsAround(cell, map[rowIndex + 1][colIndex + 1]);
+    }
+
+    // bottom
+    if (rowIndex < ROWS - 1) {
+      setBombsAround(cell, map[rowIndex + 1][colIndex]);
+    }
+
+    //bottom - left
+    if (rowIndex < ROWS - 1 && colIndex > 0) {
+      setBombsAround(cell,  map[rowIndex + 1][colIndex - 1]);
+    }
+
+    // left
+    if (colIndex > 0) {
+      setBombsAround(cell, map[rowIndex][colIndex - 1]);
+    }
+  }
+}
+
+console.log(map);
 
